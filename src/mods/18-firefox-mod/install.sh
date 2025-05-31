a452ce17e3294aaba6ff8a31d60d3fab16b2df7b
abc123-hash-boop <157674226+abc123-hash-boop@users.noreply.github.com>
2025-05-31 18:42:55 +0800

    Update install.sh

--- src/mods/18-firefox-mod/install.sh
+++ src/mods/18-firefox-mod/install.sh
@@ -1,4 +1,4 @@
 wget -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i chrome.deb
-apt --fix-broken install
+apt --fix-broken install -y
 rm chrome.deb
