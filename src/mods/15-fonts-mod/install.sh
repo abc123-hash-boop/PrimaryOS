432a9d1dcf964b535a2c21c4c946c5f627d19056
abc123-hash-boop <157674226+abc123-hash-boop@users.noreply.github.com>
2025-06-13 22:11:49 +0800

    Update install.sh

--- src/mods/15-fonts-mod/install.sh
+++ src/mods/15-fonts-mod/install.sh
@@ -2,11 +2,4 @@ set -e                  # exit on error
 set -o pipefail         # exit on pipeline error
 set -u                  # treat unset variable as error
 
-print_ok "Patching fonts..."
-cp ./local.conf /etc/fonts/
-unzip -O UTF-8 ./fonts.zip -d /usr/share/fonts/
-judge "Patch fonts"
-
-print_ok "Updating font cache"
-fc-cache -f
-judge "Update font cache"
+apt install -y ubuntu-fonts
