9d7d2590ae57b1735b9c0d995e45dfc64fc8348a
abc123-hash-boop <157674226+abc123-hash-boop@users.noreply.github.com>
2025-06-14 12:25:25 +0800

    Update install.sh

--- src/mods/14-gnome-apps-mod/install.sh
+++ src/mods/14-gnome-apps-mod/install.sh
@@ -1,6 +1,4 @@
-set -e                  # exit on error
-set -o pipefail         # exit on pipeline error
-set -u                  # treat unset variable as error
+
 
 print_ok "Installing gnome-shell and other gnome applications"
 
