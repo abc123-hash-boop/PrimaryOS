3530155cb0237e12ba55b83bfc95d393c8062a5e
abc123-hash-boop <157674226+abc123-hash-boop@users.noreply.github.com>
2025-06-15 10:55:10 +0800

    Update install.sh

--- src/mods/38-root-conf-cleanup/install.sh
+++ src/mods/38-root-conf-cleanup/install.sh
@@ -1,6 +1,3 @@
-set -e                  # exit on error
-set -o pipefail         # exit on pipeline error
-set -u                  # treat unset variable as error
 
 print_ok "Cleaning up /root/.config/ and root's gnome-shell extensions"
 /usr/bin/pipx uninstall gnome-extensions-cli
