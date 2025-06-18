d4512e55b153a41ed9a0935a76fb973041e8c2ec
abc123-hash-boop <157674226+abc123-hash-boop@users.noreply.github.com>
2025-06-18 14:10:33 +0800

    Update install.sh

--- src/mods/07-system-tools-install-mod/install.sh
+++ src/mods/07-system-tools-install-mod/install.sh
@@ -93,8 +93,6 @@ apt install $INTERACTIVE \
     eject \
     gettext \
     cracklib-runtime \
-    libfuse2t64 \
-    libfuse3-3 \
     libopengl0 \
     initramfs-tools \
     --no-install-recommends
